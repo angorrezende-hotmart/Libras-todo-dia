d84548aa7f6bcca402b65608d3b6ed86ed1c5163
iosvolpix <ios.volpix@gmail.com>
2022-08-24 15:17:49 -0300

    Novo Projeto

---
+++ Libras Todo dia/Libras Todo dia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Libras Todo dia
+//
+//  Created by Angor Volpi Silva Rezende on 24/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
